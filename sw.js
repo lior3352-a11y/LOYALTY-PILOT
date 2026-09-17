71f451f2473d396e70fb6e76f1b8feb279e521b4
lior3352-a11y <lior3352@gmail.com>
2026-09-17 09:14:54 +0300

    Bump Loyalty US cache after mobile UI fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='loyalty-pilot-v10';
+const CACHE='loyalty-pilot-v11';
 const ASSETS=['./','./index.html','./join.html','./customer.html','./accessibility.html','./terms.html','./accessibility.css','./site-common.js','./manifest.webmanifest','./icon.svg','./icon-192.svg','./icon-512.svg'];
 
 self.addEventListener('install',event=>{
